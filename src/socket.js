a647c528f5e18b45eae978569a756e922427c54e
Jatin Srivastava <137695680+JatinSri1909@users.noreply.github.com>
2024-07-07 15:36:49 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client"; // import connection function
 
-const socket = io('https://multiplayer-chess-game-havj.onrender.com/'); // initialize websocket connection
+const socket = io('https://multiplayer-chess-game-server.onrender.com/'); // initialize websocket connection
 
 export default socket;
